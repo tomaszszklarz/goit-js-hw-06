8943e176abe1daf89ea7ac58eb440a1df947d299
Tomas Szklarz <tomaszszklarz@icloud.com>
2022-07-27 12:29:22 +0200

    task-02

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const list = document.querySelector('#ingredients');
+const array = [];
+ingredients.forEach(element => {
+  const newElement = document.createElement('li');
+  newElement.textContent = element;
+  newElement.classList.add('item');
+  array.push(newElement);
+})
+list.append(...array);
