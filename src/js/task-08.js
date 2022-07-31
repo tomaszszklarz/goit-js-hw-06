f1bd8e8fb35c114a60aa7e5c0690260e91bf6edd
Tomas Szklarz <tomaszszklarz@icloud.com>
2022-07-31 11:19:15 +0200

    taks-09

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,8 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const {
+        elements: { email, password } } = e.currentTarget;
+
+})
--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,13 @@
+const btn = document.querySelector('button.change-color');
+const nameColor = document.querySelector('span.color');
+const colorBody = document.querySelector('body');
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+btn.addEventListener('click', () => {
+  colorBody.style.background = getRandomHexColor();
+  nameColor.textContent = getRandomHexColor();
+})
+
