87dcb9bd3feca2673a7fdaa51b8703d78c2e15f2
Tomas Szklarz <tomaszszklarz@icloud.com>
2022-07-31 14:09:39 +0200

    task-08

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -4,5 +4,9 @@ form.addEventListener('submit', (e) => {
     e.preventDefault();
     const {
         elements: { email, password } } = e.currentTarget;
-
-})
+    if (email.value.length === 0 || password.value.length === 0) {
+        return alert("Proszę wypełnić wymagane pola!")
+    }
+    console.log(`Login: ${email.value}, Password: ${password.value}`);
+    e.currentTarget.reset();
+});
